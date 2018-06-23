f5fe876d1a364aaeee419faac824419dc96a4167
Geetazon <geeta.puri@gmail.com>
2018-06-23 10:43:09 +0800

    CoachAppForParent

---
+++ src/components/schedule/schedule.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { AddScheduleComponent } from '../add-schedule/add-schedule';
+import { NavController } from 'ionic-angular';
+import { GetDataFromSpringProvider} from '../../providers/get-data-from-spring/get-data-from-spring';
+import { EditScheduleDetailsComponent } from '../edit-schedule-details/edit-schedule-details';
+/**
+ * Generated class for the ScheduleComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'schedule',
+  templateUrl: 'schedule.html'
+})
+export class ScheduleComponent {
+
+  text: string;
+  scheduleList = [];
+  myDate: String = new Date().toISOString();
+  groupList =[];
+  public selectedGroup;
+  public selectedKid;
+  public kidsList;
+
+  constructor(private springData: GetDataFromSpringProvider, public navCtrl: NavController, ) {
+    console.log('Hello ScheduleComponent Component');
+    this.text = 'Hello World Schedule Component';
+  }
+
+  public onItemSelection(selection){
+    let item=this.selectedKid;
+    if (selection!=undefined){
+      console.log("item selected: "+item.kidName );
+
+    }
+  }
+
+  getKids(){
+    this.springData.getKidInfoParent().subscribe(
+      data => {
+
+        this.kidsList= data.kidList;
+        this.selectedKid= data.kidList[0];
+
+      },
+      err => console.error(err),
+      () => console.log('getKidInfoParent completed')
+    )
+  }
+
+  getSchedule(){
+
+        this.springData.getScheduleKid(this.selectedKid.kidID).subscribe(
+          data => {
+
+
+            this.scheduleList= data.Schedule;
+          },
+          err => console.error(err),
+          () =>
+            console.log('get schedule Kid completed'),
+        );
+      }
+
+
+  addSchedule(){
+
+    this.navCtrl.push(AddScheduleComponent, {});
+
+  }
+
+  goToEditScheduleDetails(item){
+
+    this.navCtrl.push(EditScheduleDetailsComponent, {item: item});
+
+  }
+
+
+
+
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,78 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController , NavParams} from 'ionic-angular';
+import { GetDataFromSpringProvider} from '../../providers/get-data-from-spring/get-data-from-spring';
+
+import { Observable } from 'rxjs/Rx';
+import { ScheduleComponent } from '../../components/schedule/schedule';
+import { AttendanceComponent } from '../../components/attendance/attendance';
+import { FeesComponent } from '../../components/fees/fees';
+import { KidsComponent } from '../../components/kids/kids';
+import { PerformanceComponent } from '../../components/performance/performance';
+import { EventsComponent } from '../../components/events/events';
+import { ManageClassesComponent } from '../../components/manage-classes/manage-classes';
+import { ClassesComponent} from '../../components/classes/classes';
+import { GroupsComponent} from '../../components/groups/groups';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  ngOnInit(){
+    console.log("will call get parentID");
+    this.springData.getParentID(this.user).subscribe(
+      data => {
+        console.log("in subscribe to data of getParentID");
 
-  constructor(public navCtrl: NavController) {
+        this.parent= data.parent;
+        console.log("parent id received as : " + data.parent[0].parentID);
+        //console.log("size = " + data.length);
+      },
+      err => console.error(err),
+      () =>
+        console.log('getParentID completed'),
+    );
+  }
+
+  public user;
+  public parent;
+
+
+  constructor(private springData: GetDataFromSpringProvider,public navCtrl: NavController, public navParams: NavParams) {
+    this.user= navParams.get('role');
+    console.log('received on home page, username = ' + this.user);
+  }
+  goToSchedule(){
+
+    this.navCtrl.push(ScheduleComponent, {});
+  }
+  goToAttendance(){
+
+    this.navCtrl.push(AttendanceComponent);
 
   }
+  goToFees(){
+
+    this.navCtrl.push(FeesComponent);
+  }
+  getKids(){
+    console.log("in kids");
+    this.navCtrl.push(KidsComponent);
+  }
+
+  goToManageGroups(){
+    console.log("manage groups");
+    this.navCtrl.push(GroupsComponent);
+  }
+  goToPerformance(){
+    console.log("in performance");
+    this.navCtrl.push(PerformanceComponent);
+  }
+  goToEvents(){
+    console.log("in events");
+    this.navCtrl.push(EventsComponent);
+  }
+
 
 }
